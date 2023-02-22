bb27e3ac45b5632f3516bfd571d0f47789917d65
Helielton Fioramonte <helifiora@gmail.com>
2023-02-22 14:01:52 -0300

    chore: analyzing checkmate, initial state

--- pychess/game.py
+++ pychess/game.py
@@ -1,18 +1,9 @@
-from typing import Protocol, Iterable, Iterator
+from typing import Callable, Protocol, Iterable, Iterator
 
 from pychess.color import Color
 from pychess.board import Board
 from pychess.position import Position
 from pychess.piece import Rook, Knight, King, Bishop, Pawn, Queen, Piece
-
-
-class PieceGame(Protocol):
-    @property
-    def color(self) -> Color:
-        raise NotImplemented()
-
-    def movements(self) -> Iterable[Position]:
-        raise NotImplemented()
 
 
 class NoPieceInPositionError(Exception):
@@ -36,12 +27,14 @@ class Game:
     __turn: Color
     __captured: set[Piece]
     __check: Color | None
+    __checkmate: Color | None
 
     def __init__(self, *, board: Board | None = None, turn: Color | None = None):
         self.__board = board or self.create_board()
         self.__turn = turn or Color.WHITE
         self.__captured = set()
         self.__check = None
+        self.__checkmate = None
 
     @property
     def turn(self) -> Color:
@@ -55,6 +48,26 @@ class Game:
     def captured(self) -> set[Piece]:
         return self.__captured
 
+    def play(self, source: Position, target: Position) -> None:
+        origin_piece = self.__select_source_piece(source)
+        target_piece = self.__select_target_piece(target)
+
+        if Game.is_move_apply_check(self.__board, self.__turn, source, target):
+            raise PieceMoveCauseCheckError()
+
+        origin_piece.move(target)
+
+        self.__capture_piece(target_piece)
+        self.__change_turn()
+
+        if not self.is_check(self.__board, self.__turn):
+            return
+
+        if self.is_checkmate(self.__board, self.__turn):
+            self.__checkmate = self.__turn
+        else:
+            self.__check = self.__turn
+
     def select_piece_moves(self, position: Position) -> list[Position]:
         piece = self.__board.get_piece(position)
         if piece is None:
@@ -64,26 +77,20 @@ class Game:
             raise AnotherTeamTurnError()
 
         def wrapper(new_position: Position) -> bool:
-            return not self.__is_move_apply_check(position, new_position)
+            return not Game.is_move_apply_check(
+                self.__board, self.__turn, position, new_position
+            )
 
         return list(filter(wrapper, piece.movements()))
 
-    def play(self, source: Position, target: Position) -> None:
-        piece = self.__select_source_piece(source)
-        old_value = self.__select_target_piece(target)
+    def __capture_piece(self, piece: Piece | None) -> None:
+        if piece is None or piece.onboard:
+            return
 
-        if self.__is_move_apply_check(source, target):
-            raise PieceMoveCauseCheckError()
+        self.__captured.add(piece)
 
-        piece.move(target)
-
-        if old_value and old_value.outboard:
-            self.__captured.add(old_value)
-
-        self.__change_turn()
-
-        if self.is_check(self.__board.clone(), self.__turn):
-            self.__check = self.__turn
+    def __change_turn(self) -> None:
+        self.__turn = self.__turn.inverse()
 
     def __select_source_piece(self, source: Position) -> Piece:
         piece = self.__board.get_piece(source)
@@ -98,23 +105,8 @@ class Game:
 
         return piece
 
-    def __change_turn(self) -> None:
-        self.__turn = self.__turn.inverse()
-
     def __select_target_piece(self, target: Position) -> Piece | None:
         return self.__board.get_piece(target)
-
-    def __is_move_apply_check(self, origin: Position, target: Position) -> bool:
-        board_copy = self.__board.clone()
-        piece_copy = board_copy.get_piece(origin)
-        if piece_copy is None:
-            return False
-
-        if not piece_copy.can_move_to(target):
-            return False
-
-        board_copy.place(piece_copy, target)
-        return self.is_check(board_copy, self.__turn)
 
     @staticmethod
     def is_check(board: Board, color: Color) -> bool:
@@ -125,6 +117,36 @@ class Game:
             return king_position in piece.movements()
 
         return any(map(is_king_in_movements, opponent_pieces))
+
+    @staticmethod
+    def is_checkmate(board: Board, color: Color) -> bool:
+        def not_apply_check(origin: Position) -> Callable[[Position], bool]:
+            def wrapper(target: Position) -> bool:
+                return not Game.is_move_apply_check(board, color, origin, target)
+
+            return wrapper
+
+        moves_that_undo_check = (
+            map(not_apply_check(piece.position), piece.movements())
+            for piece in board.get_pieces(color)
+        )
+
+        return not any(moves_that_undo_check)
+
+    @staticmethod
+    def is_move_apply_check(
+        board: Board, color: Color, origin: Position, target: Position
+    ) -> bool:
+        board_copy = board.clone()
+        piece_copy = board_copy.get_piece(origin)
+        if piece_copy is None:
+            return False
+
+        if not piece_copy.can_move_to(target):
+            return False
+
+        board_copy.place(piece_copy, target)
+        return Game.is_check(board_copy, color)
 
     @staticmethod
     def create_board() -> Board:
